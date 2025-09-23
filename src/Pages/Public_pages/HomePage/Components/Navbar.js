6c84f81d12602772c5b34ff18b05169d5b57a631
Shyam Agarwal <shyam.agarwal.profess@gmail.com>
2025-09-23 17:56:22 +0530

    resolved navbar issue

--- src/Pages/Public_pages/HomePage/Components/Navbar.js
+++ src/Pages/Public_pages/HomePage/Components/Navbar.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import Navlinks from '../../../../Data/Navlinks.js';
+import Navlinks from '../../../../Data/navlinks.js';
 import { useLocation, matchPath, Link } from 'react-router-dom';
 import { useDispatch, useSelector } from 'react-redux';
 import ProfileDropDown from '../../../../components/ProfileComponents/ProfileDropDown.jsx';
