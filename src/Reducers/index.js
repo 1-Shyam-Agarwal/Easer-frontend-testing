9195407292c1e5dff6fd1638ff7e40b4ffc24618
Shyam Agarwal <shyam.agarwal.profess@gmail.com>
2025-09-23 16:56:12 +0530

    Fix AuthSlice filename casing

--- src/Reducers/index.js
+++ src/Reducers/index.js
@@ -1,8 +1,8 @@
 import { combineReducers } from '@reduxjs/toolkit';
 import authReducer from '../Slices/AuthSlice.js';
 import userReducer from '../Slices/profileSlice.js';
-import logoutReducer from '../Slices/LogoutSlice.jsx';
-import disableReducer from '../Slices/DisableFunctionality.jsx';
+import logoutReducer from '../Slices/LogoutSlice.js';
+import disableReducer from '../Slices/DisableFunctionality.js';
 
 const rootReducer = combineReducers({
   auth: authReducer,
